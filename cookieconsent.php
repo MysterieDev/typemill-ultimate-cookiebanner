95774ab53c71c87b5517579c94bf04b735a5a2c0
Mysteriedev <josefsle97@gmail.com>
2021-08-23 13:20:42 +0200

    init

---
+++ cookieconsent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Plugins\CookieConsent;
+
+use \Typemill\Plugin;
+
+class CookieConsent extends Plugin
+{
+	protected $settings;
+	
+    public static function getSubscribedEvents()
+    {
+		return array(
+			'onSettingsLoaded'		=> 'onSettingsLoaded',
+			'onTwigLoaded' 			=> 'onTwigLoaded'
+		);
+    }
+	
+	public function onSettingsLoaded($settings)
+	{
+		$this->settings = $settings->getData();
+	}
+	
+	public function onTwigLoaded()
+	{
+		/* add external CSS and JavaScript */
+		$this->addCSS('/cookieconsent/public/solid-css.css');	
+
+		/* get Twig Instance and add the cookieconsent template-folder to the path */
+		$twig 	= $this->getTwig();					
+		$loader = $twig->getLoader();
+		$loader->addPath(__DIR__ . '/templates');
+	
+		/* fetch the template, render it with twig and add it as inline-javascript */
+		$this->addInlineJS($twig->fetch('/cookieconsent.twig', $this->settings));
+	}
+}
